25bc45e4978050c6deb610be617fb394eb8516b9
Prabal Negi <negi@kth.se>
2017-05-14 02:55:38 +0200

    updating from gimpel

--- scripts/simson_bl/make_streamlines.m
+++ scripts/simson_bl/make_streamlines.m
@@ -9,7 +9,6 @@ filename='xy.stat';
 
 [X,Y]=meshgrid(x,y);
 
-
 x_start = 1000;
 x_end   = 11000;
 y_start = 0;
@@ -25,8 +24,12 @@ y2 = y(indy);
 
 [X2,Y2]=meshgrid(x2,y2);
 
+total_p = 0.5*(stat.u2 + stat.v2 + stat.w2) + stat.p;
+
 figure(100)
-surf(X2,Y2,stat.u(indy,indx),'EdgeColor', 'none', 'FaceColor', 'interp');
+%surf(X2,Y2,stat.u(indy,indx),'EdgeColor', 'none', 'FaceColor', 'interp');
+%surf(X2,Y2,atan(stat.v(indy,indx)./stat.u(indy,indx))*180/pi,'EdgeColor', 'none', 'FaceColor', 'interp');
+surf(X2,Y2,total_p(indy,indx),'EdgeColor', 'none', 'FaceColor', 'interp');
 view(2)
 colorbar
 hold on
@@ -36,6 +39,8 @@ delta99 = [];
 for i=1:length(x)
   delta99(i)=interp1(stat.u(:,i),y,bl_criteria);
   delta995(i)=interp1(stat.u(:,i),y,0.995);
+  Ue(i)=interp1(y,stat.u(:,i),delta99(i));
+  Ve(i)=interp1(y,stat.v(:,i),delta99(i));
 end
 
 figure(100)
@@ -64,51 +69,48 @@ figure(2)
 plot(x,dstar./theta)
 xlim([1000 12000])
 
-x_seed = 1000; 
-nseed = 5;
-dr = 1e-2;
-maxverts=828000;
-blh_xin = interp1(x,delta99,x_seed);
-y_seed = linspace(1e-4,1.0*blh_xin,nseed);
-
-x_sl = [];
-y_sl = [];
-y_sl2 = [];
-xsl_max = 12000;
-ysl_max = 400;
-
-for iseed = nseed:-1:1
-  xsl=x_seed;
-  ysl=y_seed(iseed);
-  xsl_arr = [xsl];
-  ysl_arr = [ysl];
-  npts=0;    
-  while ((xsl<xsl_max) && (ysl<ysl_max))
-    clc  
-    npts=npts+1
-    iseed  
-    [xsl ysl] = sl_rk4(X,Y,stat.u,stat.v,dr,xsl,ysl);
-    xsl_arr = [xsl_arr; xsl];
-    ysl_arr = [ysl_arr; ysl];
+%x_seed = 1000; 
+%nseed = 1;
+%dr = 1e-2;
+%maxverts=828000;
+%blh_xin = interp1(x,delta99,x_seed);
+%%y_seed = linspace(1e-4,1.0*blh_xin,nseed);
+%y_seed = 1.25*blh_xin;
+%
+%x_sl = [];
+%y_sl = [];
+%y_sl2 = [];
+%xsl_max = 12000;
+%ysl_max = 400;
+%
+%for iseed = 1:nseed
+%  xsl=x_seed;
+%  ysl=y_seed(iseed);
+%  xsl_arr = [xsl];
+%  ysl_arr = [ysl];
+%  npts=0;    
+%  while ((xsl<xsl_max) && (ysl<ysl_max))
+%    clc  
+%    npts=npts+1;
+%    iseed  
+%    [npts xsl]
+%    [xsl ysl] = sl_rk4(X,Y,stat.u,stat.v,dr,xsl,ysl);
+%    xsl_arr = [xsl_arr; xsl];
+%    ysl_arr = [ysl_arr; ysl];
 %    if (mod(npts,1000)==0)
 %      figure(20)
 %      plot(xsl_arr,ysl_arr)
 %      pause(0.1)
 %    end    
-  end
-  slines(iseed).x=xsl_arr;
-  slines(iseed).y=ysl_arr;
-  slines(iseed).xw = x;
-  slines(iseed).delta99 = delta99;        
-end     
- 
-
-
-
-ind1=x_sl>=x_start;
-ind2=x_sl<=x_end;
-
-save('slines.mat', 'slines')
+%  end
+%  slines(iseed).x=xsl_arr;
+%  slines(iseed).y=ysl_arr;
+%  slines(iseed).xw = x;
+%  slines(iseed).delta99 = delta99;        
+%end     
+%
+%
+%save('slines.mat', 'slines')
 
 % figure(100)
 % for i=1:nseed
